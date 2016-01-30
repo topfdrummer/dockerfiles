0248ec69a8ad8013b0fb2e2eec89ce29d8a5e150
Fabian <topfdrummer@users.noreply.github.com>
2016-01-30 16:10:32 +0100

    update install.sh: move direwolf20 to 1.10
    Moved from direwolf20 1.8 to 1.10

--- minecraft/1.7.10.direwolf/bin/install.sh
+++ minecraft/1.7.10.direwolf/bin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /srv/minecraft
-wget -O server.zip ftb.cursecdn.com/FTB2/modpacks/direwolf20_17/1_8_0/direwolf20_17-server.zip
+wget -O server.zip ftb.cursecdn.com/FTB2/modpacks/direwolf20_17/1_10_0/direwolf20_17-server.zip
 unzip server.zip
 rm server.zip
 rm *.bat
