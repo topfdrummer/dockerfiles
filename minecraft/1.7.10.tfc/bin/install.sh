770ab0e49d9f9d827c06d2b3afc0e73a65220ed2
Fabian <topfdrummer@googlemail.com>
2016-06-18 14:26:40 +0200

    change to tfc 79-29 with forge 10.13.4.1558

--- minecraft/1.7.10.tfc/bin/install.sh
+++ minecraft/1.7.10.tfc/bin/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-wget -O /srv/minecraft/forgeinstaller.jar http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.4.1448-1.7.10/forge-1.7.10-10.13.4.1448-1.7.10-installer.jar
+wget -O /srv/minecraft/forgeinstaller.jar http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.4.1558-1.7.10/forge-1.7.10-10.13.4.1558-1.7.10-installer.jar
 chmod +x /srv/minecraft/forgeinstaller.jar
 cd /srv/minecraft
 java -jar /srv/minecraft/forgeinstaller.jar --installServer
 mkdir /srv/minecraft/mods
-wget -O /srv/minecraft/mods/tfc.jar https://bitly.com/Build79-25
-wget -O /srv/minecraft/mods/fastcraft-1.21.jar http://files.player.to/fastcraft-1.21.jar
+wget -O /srv/minecraft/mods/tfc.jar https://bitly.com/Build79-29
+wget -O /srv/minecraft/mods/fastcraft-1.23.jar http://files.player.to/fastcraft-1.23.jar
 
 rm /srv/minecraft/forgeinstaller.jar
 
-cp /srv/minecraft/forge-1.7.10-10.13.4.1448-1.7.10-universal.jar /srv/minecraft/forge.jar
+cp /srv/minecraft/forge-1.7.10-10.13.4.1558-1.7.10-universal.jar /srv/minecraft/forge.jar
 
 chmod +x /srv/minecraft/forge.jar
 
