d82492f8c1b15625212948e654dddbb7fd56c7c5
Fabian <topfdrummer@googlemail.com>
2016-06-25 11:48:53 +0200

    added Dockerfile and Co for TerraFirmaPunk

---
+++ minecraft/tfp/bin/install.sh
@@ -0,0 +1,12 @@
+cd /srv
+wget -O server.zip ftb.cursecdn.com/FTB2/modpacks/TerraFirmaPunk/2_0_3/TerraFirmaPunkServer.zip
+#wget -O server.zip ftb.cursecdn.com/FTB2/modpacks/TerraFirmaPunk/2_0_0/TerraFirmaPunkServer.zip
+unzip /srv/server.zip
+rm server.zip
+cd /srv/minecraft
+rm *.bat
+rm *.exe
+echo eula=true > /srv/minecraft/eula.txt
+chmod +x /srv/minecraft/ServerStart-Other.sh
+chmod +x forge-1.7.10-10.13.4.1566-1.7.10-installer.jar
+java -jar /srv/minecraft/forge-1.7.10-10.13.4.1566-1.7.10-installer.jar --installServer
